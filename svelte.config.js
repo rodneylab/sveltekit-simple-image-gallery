76e8afc72b33c8c3c7cf202b247d50694c1dacdb
Rodney Lab <codesign@rodneylab.com>
2022-04-22 09:29:35 +0100

    fix: 💫 update SvekteKit config

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,7 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
   kit: {
     adapter: adapter(),
+    prerender: { default: true },
     vite: {
       plugins: [imagetools({ force: true })],
     },
